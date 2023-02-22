6ff4c6e846af80351eb452bda1c5280d63ed220d
mvacjar <mvacjar@gmail.com>
2023-02-22 18:14:25 +0100

    API changed

--- src/app.js
+++ src/app.js
@@ -73,9 +73,15 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(coordinates) {
+/*function getForecast(coordinates) {
   let apiKey = "bc92040c41ead89e1ebda9b28b14ef5b";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}*/
+
+function getForecast(coordinates) {
+  let apiKey = "bd3bb6534458ba51b48c49f5155745b6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
